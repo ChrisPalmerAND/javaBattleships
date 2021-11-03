ef0500d4b0afaeeb55b2762e3e1ff7c937b34b70
Chris Palmer <chris.palmer@and.digital>
2021-11-03 16:53:08 +0000

    commiting files

---
+++ CheckGuess.java
@@ -0,0 +1,12 @@
+public class CheckGuess {
+    public boolean checkGuess(String guess, Battleship b1, Battleship b2, Battleship b3){
+        boolean b1Check = b1.adjustLocationArray(guess);
+        boolean b2Check = b2.adjustLocationArray(guess);
+        boolean b3Check = b3.adjustLocationArray(guess);
+
+        if (b1Check == false && b2Check == false && b3Check == false){
+            return false;
+        }
+        return true;
+    }
+}
